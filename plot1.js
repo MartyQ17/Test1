efbd7227f352ca1d1073c242e2345fbd92a30be5
MartyQ17 <martina.quaregna.17@gmail.com>
2022-12-17 16:42:55 +0100

    no major updates, only a few hings :>

--- plot1.js
+++ plot1.js
@@ -1,11 +1,12 @@
 
-
 function rand() {
   return Math.random();
 }// genero numeri randomici
+// ---> Qui andra' inserito l'ingresso dei dai e rendenzialmente, per ogni grafico,
+//      andremo a lavorare con i prototipi generati in questo codice
+
 
 var time = new Date();
-
 
 var data = [{
   x: [time],
@@ -39,32 +40,40 @@ var interval = setInterval(function () {
     } //quello che visualizzo sull'asse delle x
   }; //l'aggiornamento
 
-  Plotly.relayout('myDiv', secondsView); //faccio l'update del grafico
+ // Plotly.relayout('myDiv', secondsView); //faccio l'update del grafico
   Plotly.extendTraces('myDiv', update, [0]) //aggiungo i nuovi dati al grafico esistente
 
   if (cnt === 1000) clearInterval(interval); //quando viene raggiunto il counter il grafico viene "ripulito"
-}, 100); //tutta questa parte fa in modo da aggiornare il grafico sulla base del primo elemento creato
+}, 1000); //tutta questa parte fa in modo da aggiornare il grafico sulla base del primo elemento creato
 
 
 
 
 
 //Creo una gauge chart basata su dati vari (test)
-var data1 = [{
+
+var gauge_1 = setInterval(gauge_one, 1000);
+function gauge_one () {
+  var data1 = [{
   domain: { x: [0, 1], y: [0, 1] },
   value: rand(),
   title: { text: "Numeri random" },
   type: "indicator",
   mode: "gauge+number+delta", //number -> mostra il valore reggiunto
-  delta: {reference: 0.999}, //delta -> mostra la differenza tra il target e il valore attuale
+  delta: {reference: 0.999},  //delta -> mostra la differenza tra il target e il valore attuale
   gauge: {
     axis: {range: [null, 0.999]},
     steps: [
-      {range: [0, 0.25], color: "lightgreen"},
-      {range: [0.25, 0.4], color: "yellow"},
-      {range: [0.4, 0.7], color: "orange"},
-      {range: [0.7, 0.9], color: "red"},
-      {range: [0.9, 0.999], color: "black"}
+      {range: [0, 0.25], 
+       color: "lightgreen"},
+      {range: [0.25, 0.4], 
+       color: "yellow"},
+      {range: [0.4, 0.7], 
+       color: "orange"},
+      {range: [0.7, 0.9], 
+       color: "red"},
+      {range: [0.9, 0.999], 
+       color: "black"}
     ],
     threshold: {
       line: {color: "purple", width: 4},
@@ -74,18 +83,10 @@ var data1 = [{
   }
 }];
 var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-var test2 = Plotly.newPlot('gaugeChart', data1, layout);
+var test2 = Plotly.newPlot('gaugeChart', data1, layout); 
+} // questa funzione permette l'aggiornamento del gauge chart ogni tot ms
 
 
-var data2 =  [{
-  domain: { x: [0, 1], y: [0, 1] },
-  value: rand(),
-  title: { text: "Numeri random" },
-  type: "indicator",
-  mode: "gauge+number+delta", //number -> mostra il valore reggiunto
-  delta: {reference: 0.999}, //delta -> mostra la differenza tra il target e il valore attuale
-  gauge: {shape: "bullet"}   
-}];
 
-var layout = { width: 400, height: 200, margin: { t: 0, b: 0 } };
-var test2 = Plotly.newPlot('gauge2', data2, layout);
+
+
